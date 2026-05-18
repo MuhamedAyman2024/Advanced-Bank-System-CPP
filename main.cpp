eeb614583df3b0e61bc5a0af05012fa77f19fb1d
Moe <mygit2024@gmail.com>
2026-05-19 00:50:05 +0300

    Edit: main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,6 @@
 
 
 int main() {
-  // Account acc1 {};
-  // try
-  // {
-  //   acc1.deposit(2500);
-  //   acc1.withdraw(900);
-  // }
-  // catch(const std::exception& e)
-  // {
-  //   std::cerr << e.what() << '\n';
-  // }
-  
+
   return 0;
 }
